d81997a8a5515ce916b072594fb7bbad061b4db2
서현규 <omnipede@coinplug.com>
2021-05-03 18:36:43 +0900

    기능: 스케쥴링 기능 추가
    1분에 한번 주기로 블록체인을 검사하여 DID 발급 정보를 업데이트 하도록 함.

--- src/main/java/io/omnipede/data/didpipeline/DidPipelineApplication.java
+++ src/main/java/io/omnipede/data/didpipeline/DidPipelineApplication.java
@@ -6,16 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Slf4j
+@EnableScheduling
 @SpringBootApplication
 public class DidPipelineApplication implements CommandLineRunner {
 
     private final DidPipeline didPipeline;
 
+    private final ThreadPoolTaskScheduler threadPoolTaskScheduler;
+
+    private static final int ONE_MINUTE = 60 * 1000;
+
     @Autowired
-    public DidPipelineApplication(DidPipeline didPipeline) {
+    public DidPipelineApplication(DidPipeline didPipeline, ThreadPoolTaskScheduler threadPoolTaskScheduler) {
         this.didPipeline = didPipeline;
+        this.threadPoolTaskScheduler = threadPoolTaskScheduler;
     }
 
     public static void main(String[] args) {
@@ -24,12 +33,20 @@ public class DidPipelineApplication implements CommandLineRunner {
 
     /**
      * Application 이 시작하면 현재 data warehouse 에 저장된 마지막 block 부터
-     * block chain 의 가장 마지막 블록까지 탐색하여 did 발급 정보를 동기화시킨다
+     * block chain 의 가장 마지막 블록까지 탐색하여 did 발급 정보를 동기화시킨다. 동기화 이후 스케쥴링을 시작한다.
      */
     @Override
     public void run(String... args) throws Exception {
 
         log.info("DID pipeline is executing ...");
+
+        log.info("[*] Synchronizing starts. And this job would take a long time ...");
         didPipeline.sync();
+        log.info("[*] Synchronizing ends.");
+
+        // Do scheduling every minutes
+        log.info("[*] Scheduling starts for every {} ms", ONE_MINUTE);
+        threadPoolTaskScheduler
+                .scheduleAtFixedRate(didPipeline::sync, ONE_MINUTE);
     }
 }
--- src/main/java/io/omnipede/data/didpipeline/system/config/BeanConfig.java
+++ src/main/java/io/omnipede/data/didpipeline/system/config/BeanConfig.java
@@ -2,6 +2,7 @@ package io.omnipede.data.didpipeline.system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 
@@ -11,5 +12,17 @@ class BeanConfig {
     @Bean
     public Web3j web3j() {
         return Web3j.build(new HttpService("https://api.metadium.com/prod"));
+    }
+
+    /**
+     * Bean for task scheduling
+     * @return Task scheduler bean
+     */
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(1);
+        threadPoolTaskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
+        return threadPoolTaskScheduler;
     }
 }
