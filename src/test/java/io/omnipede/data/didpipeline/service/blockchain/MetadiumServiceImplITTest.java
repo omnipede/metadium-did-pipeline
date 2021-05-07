266e445771f582ef6e4cbbebc41b47e13717d268
서현규 <omnipede@coinplug.com>
2021-05-07 14:50:37 +0900

    테스트: 변화된 blockchainService 인터페이스 메소드 시그니쳐에 맞춰 테스트 코드 수정

--- src/test/java/io/omnipede/data/didpipeline/service/blockchain/MetadiumServiceImplITTest.java
+++ src/test/java/io/omnipede/data/didpipeline/service/blockchain/MetadiumServiceImplITTest.java
@@ -1,10 +1,9 @@
 package io.omnipede.data.didpipeline.service.blockchain;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
 
 import java.util.List;
 
@@ -13,12 +12,16 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 /**
  * Metadium blockchain 관련 API 통합 테스트 코드
  */
-@SpringBootTest
-@RunWith(SpringRunner.class)
 public class MetadiumServiceImplITTest {
 
-    @Autowired
-    private MetadiumServiceImpl metadiumService;
+    private static MetadiumServiceImpl metadiumService;
+
+    @BeforeAll
+    public static void setup() {
+
+        Web3j web3j = Web3j.build(new HttpService("https://api.metadium.com/prod"));
+        metadiumService = new MetadiumServiceImpl(web3j);
+    }
 
     /**
      * 적절한 수의 DID 발급 데이터를 반환하는지 테스트한다
@@ -28,12 +31,29 @@ public class MetadiumServiceImplITTest {
 
         // Given
         long from = 10010000L;
+        long to = from + 1000 - 1;
 
         // When
-        List<DidIssuanceInfo> didIssuanceInfoList = metadiumService.getIdentityCreationEventsFrom(from);
+        List<DidIssuanceInfo> didIssuanceInfoList = metadiumService.getIdentityCreationEventsBetween(from, to);
 
         // Then
         assertThat(didIssuanceInfoList).isNotNull();
         assertThat(didIssuanceInfoList).hasSize(6);
+    }
+
+    /**
+     * Metadium block chain 의 latest block 을 반환해야 한다.
+     */
+    @Test
+    public void should_return_valid_last_block_number() throws Exception {
+
+        // Given
+        long previousLastBlock = 18225909L;
+
+        // When
+        long lastBlockNumber = metadiumService.getLatestBlock();
+
+        // Then
+        assertThat(lastBlockNumber).isGreaterThan(previousLastBlock);
     }
 }
