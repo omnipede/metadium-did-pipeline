e338f508966fdd77ce7a2f01d941803fcc39d9e2
서현규 <omnipede@coinplug.com>
2021-05-12 14:49:31 +0900

    테스트: Spring boot application 클래스 테스트코드 수정
    Elasticsearch auto configuration 에 대해 mocking 하지 않아서
    Elasticsearch server 가 존재하지 않는 환경에서는 에러가 발생하는 문제가 있엇다.
    이를 해결하기 위해 elasticsearch repository 에 대한 mock bean 을 만들도록 테스트코드를 수정함

--- src/test/java/io/omnipede/data/didpipeline/DidPipelineApplicationTest.java
+++ src/test/java/io/omnipede/data/didpipeline/DidPipelineApplicationTest.java
@@ -1,39 +1,93 @@
 package io.omnipede.data.didpipeline;
 
 import io.omnipede.data.didpipeline.application.DidPipeline;
+import io.omnipede.data.didpipeline.service.warehouse.DataWarehouseService;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
 public class DidPipelineApplicationTest {
 
-    @MockBean
-    private DidPipeline didPipeline;
+    @Nested
+    @ExtendWith(SpringExtension.class)
+    @SpringBootTest
+    @EnableAutoConfiguration(exclude = ElasticsearchDataAutoConfiguration.class)
+    public class WiredUnitTest {
 
-    @SpyBean
-    private ThreadPoolTaskScheduler scheduler;
+        @MockBean(name = "didIssuanceElasticsearchRepository")
+        private ElasticsearchRepository<?, ?> elasticsearchRepository;
 
-    @Test
-    public void scheduler_should_be_created_and_scheduling_method_should_be_called() {
+        @MockBean(name = "elasticsearchService")
+        private DataWarehouseService dataWarehouseService;
+
+        @MockBean
+        private DidPipeline didPipeline;
+
+        @SpyBean
+        private ThreadPoolTaskScheduler scheduler;
+
+        @Test
+        public void scheduler_should_be_created_and_scheduling_method_should_be_called() {
+
+            // Given
+            long period = 10 * 1000L;
+
+            // When
+
+            // Then
+            verify(scheduler, times(1))
+                    .scheduleAtFixedRate(any(), eq(period));
+            verify(didPipeline, times(1))
+                    .process();
+        }
+    }
+
+    @Nested
+    public class NonWiredUnitTest {
 
         // Given
+        MockedStatic<SpringApplication> mockedStatic;
 
-        // When
-        // Below code is just for coverage ...
-        DidPipelineApplication.main(new String[]{});
+        @BeforeEach
+        public void setup() {
 
-        // Then
-        verify(scheduler, times(1))
-                .scheduleAtFixedRate(any(), eq(10 * 1000L));
-        verify(didPipeline, atLeastOnce())
-                .process();
+            mockedStatic = mockStatic(SpringApplication.class);
+        }
+
+        @AfterEach
+        public void cleanup() {
+
+            mockedStatic.close();
+        }
+
+        @Test
+        public void main_method_coverage() {
+            // Given
+
+            // When
+            DidPipelineApplication.main(new String[]{});
+
+            // Then
+            mockedStatic.verify(() -> {
+                SpringApplication.run(eq(DidPipelineApplication.class), any());
+            }, times(1));
+        }
     }
 }
